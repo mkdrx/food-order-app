952901af499eb5255f0110da8fc7d3e573741483
Markus D <markusdrx@gmail.com>
2022-05-30 18:38:09 +0200

    added Cart and Modal components - logic pending

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+// Import to use Portals
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+// Helper to get the id
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    // Portaling - select what to portal and where (Component, location)
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
